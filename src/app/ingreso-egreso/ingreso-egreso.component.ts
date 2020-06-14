baaf29580cb22eab323376a513c8362de6740992
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-14 12:38:43 -0500

    Optimize UI Loading

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { IngresoEgresoService } from './ingreso-egreso.service';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { Subscription } from 'rxjs';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
 
 @Component({
     selector: 'app-ingreso-egreso',
     templateUrl: './ingreso-egreso.component.html',
     styles: [],
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
     forma: FormGroup;
     tipo = 'ingreso';
 
-    constructor(public ingresoEgresoService: IngresoEgresoService) {}
+    loadingSubs: Subscription = new Subscription();
+    cargando: boolean;
+
+    constructor(
+        public ingresoEgresoService: IngresoEgresoService,
+        private store: Store<AppState>
+    ) {}
 
     ngOnInit(): void {
+        this.loadingSubs = this.store
+            .select('ui')
+            .subscribe((ui) => (this.cargando = ui.isLoading));
+
         this.forma = new FormGroup({
             descripcion: new FormControl('', Validators.required),
             monto: new FormControl(0, Validators.min(0)),
         });
     }
+    ngOnDestroy(){
+        this.loadingSubs.unsubscribe();
+    }
 
     crearIngreso() {
+        this.store.dispatch(new ActivarLoadingAction());
+
         const ingresoEgreso = new IngresoEgreso({
             ...this.forma.value,
             tipo: this.tipo,
@@ -31,15 +50,14 @@ export class IngresoEgresoComponent implements OnInit {
         this.ingresoEgresoService
             .crearIngresoEgreso(ingresoEgreso)
             .then(() => {
-                Swal.fire(
-                    'Creado',
-                    ingresoEgreso.descripcion,
-                    'success'
-                );
+                this.store.dispatch(new DesactivarLoadingAction());
+                Swal.fire('Creado', ingresoEgreso.descripcion, 'success');
                 this.forma.reset({
                     monto: 0,
                 });
             })
-            .catch();
+            .catch(err => {
+                this.store.dispatch(new DesactivarLoadingAction());
+            });
     }
 }
--- src/app/ingreso-egreso/ingreso-egreso.model.ts
+++ src/app/ingreso-egreso/ingreso-egreso.model.ts
@@ -2,12 +2,10 @@ export class IngresoEgreso {
     public descripcion: string;
     public monto: number;
     public tipo: string;
-    public uid?: string;
     constructor(obj: DataObj) {
         this.descripcion = (obj && obj.descripcion) || null;
         this.monto = (obj && obj.monto) || null;
         this.tipo = (obj && obj.tipo) || null;
-        this.uid = (obj && obj.uid) || null;
     }
 }
 
@@ -15,5 +13,4 @@ interface DataObj {
     descripcion: string;
     monto: number;
     tipo: string;
-    uid?: string;
 }
