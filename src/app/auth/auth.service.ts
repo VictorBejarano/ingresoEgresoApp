570a06b1cbd4ae1f16fa080beb0221285b5e1e11
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-14 01:33:23 -0500

    User document

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { auth } from 'firebase/app';
 
@@ -7,18 +8,34 @@ import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
 
 import Swal from 'sweetalert2';
+import { User } from './user.model';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
-    constructor(private afAuth: AngularFireAuth, private router: Router) {}
+    constructor(
+        private afAuth: AngularFireAuth,
+        private router: Router,
+        private afDB: AngularFirestore
+    ) {}
 
     crearUsuario(nombre: string, email: string, password: string) {
         this.afAuth
             .createUserWithEmailAndPassword(email, password)
             .then((res) => {
-                this.router.navigate(['/']);
+                const user: User = {
+                    uid: res.user.uid,
+                    nombre,
+                    email: res.user.email,
+                };
+
+                this.afDB
+                    .doc(`${user.uid}/usuario`)
+                    .set(user)
+                    .then(() => {
+                        this.router.navigate(['/']);
+                    });
             })
             .catch((error) => {
                 Swal.fire('Error en el login', error.message, 'error');
