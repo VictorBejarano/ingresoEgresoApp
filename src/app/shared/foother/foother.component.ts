5fa1f5c48d6aa4aa08ad9447834243109e7c4e1a
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-13 22:43:40 -0500

    Design style

---
+++ src/app/shared/foother/foother.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foother',
+  templateUrl: './foother.component.html',
+  styles: [
+  ]
+})
+export class FootherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
