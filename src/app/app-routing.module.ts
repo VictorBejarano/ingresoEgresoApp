6ce8c755afd26d6561686d45fd407117c3979e71
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-14 19:16:34 -0500

    CanLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -9,6 +10,7 @@ const routes: Routes = [
     {
         path: '',
         loadChildren: () => import(`./ingreso-egreso/ingreso-egreso.module`).then(m => m.IngresoEgresoModule),
+        canLoad: [ AuthGuardService ]
     },
     { path: '**', redirectTo: '' },
 ];
